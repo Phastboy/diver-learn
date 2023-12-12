1395756f06af71ae484aff427b3983650c19a697
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-12-12 17:38:33 +0100

    homepage partly done

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
-import Image from 'next/image'
-import Button from '@/app/ui/button'
+import Image from 'next/image';
+import Button from '@/app/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <>
-      <Image
-        src="/landing.svg"
-        alt={'Landing'}
-        width={500}
-        height={500}
-      />
-      <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <Button>Get started</Button>
+      <div className='max-h-screen'>
+        <Image
+          src="/landing.svg"
+          alt={'Landing'}
+          width={500}
+          height={500}
+        />
+        <div className="flex flex-col items-center justify-center py-2">
+          <Link href='/auth/sign-up'><Button>Get started</Button></Link>
+        </div>
       </div>
     </>
   )
