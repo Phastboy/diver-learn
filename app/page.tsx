b4ec3dd77c6ca47df155ccf629a2e77f931be944
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-12-12 16:29:25 +0100

    app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
-
+import Button from '@/app/ui/button'
 export default function Home() {
   return (
     <>
       <Image
-        src="@/public/landing.svg"
+        src="/landing.svg"
         alt={'Landing'}
         width={500}
         height={500}
       />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Button>Get started</Button>
+      </div>
     </>
   )
 }
