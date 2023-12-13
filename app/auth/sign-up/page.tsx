9cbde4d57b553f83065b66a43791e81476a5ecfd
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-12-13 15:45:30 +0100

    tried styling auth pages

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link';
+import Image from "next/image";
 import Button from '@/app/ui/button';
 
 export default function SignUp(){
   return(
-    <div>
+    <div className='justify-center items-center flex flex-col'>
+      <Image
+        src='/loginBackground.svg'
+        alt='backgroundCover'
+        fill={true}
+      />
       <h1>Sign Up</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
+      <form className='flex flex-col mt-5 space-y-3'>
+        <input type="text" placeholder="Username" className='rounded-md'/>
+        <input type="password" placeholder="Password" className='rounded-md'/>
+        <input type="password" placeholder="Confirm Password" className='rounded-md' />
         <Link href='/verify'><Button type="submit">Sign Up</Button></Link>
       </form>
       <p>Already have an account? <Link href='/auth/login'>Login</Link></p>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Courses from '@/app/ui/dashboard/courses';
 import NoCourses from '@/app/ui/dashboard/noCourses';
-import Button from '@/app/ui/button';
 import Nav from '@/app/ui/dashboard/nav';
 import { useState, useEffect } from 'react';
 
