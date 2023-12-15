192c63dd8f4d170870f9f413ed00bbe2820cc3f5
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-12-15 09:17:05 +0100

    main nav, it is not okay yet but it is presentable

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,62 @@
 'use client'
 import Courses from '@/app/ui/dashboard/courses';
 import NoCourses from '@/app/ui/dashboard/noCourses';
-import Nav from '@/app/ui/dashboard/nav';
+import Nav from '@/app/ui/nav';
 import { useState, useEffect } from 'react';
+import { fetchUser } from '../lib/userData';
 
 export default function Dashboard() {
   const [courses, setCourses] = useState(false);
-  return (
-    <div className='max-h-screen'>
-      <Nav/>
-      {courses ? <Courses/> : <NoCourses/>}
-    </div>
-  );
+  const [name, setName] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    fetchUser().then((user) => {
+      setCourses(user.courses);
+      setName(user.name);
+      setLoading(false);
+    }).catch((e) => {
+      setError(true);
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <Nav />
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="text-2xl font-bold">Loading...</h1>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <Nav />
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="text-2xl font-bold">Error</h1>
+        </div>
+      </div>
+    );
+  } else if (courses) {
+    return (
+      <div>
+        <Nav />
+        <h1 className="text-2xl font-bold text-center mt-8">Welcome, {name}!</h1>
+        <Courses courses={courses} name={name} />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Nav />
+        <h1 className="text-2xl font-bold text-center mt-8">Welcome, {name}!</h1>
+        <NoCourses />
+      </div>
+    );
+  }
 }
--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -1,29 +1,43 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Home, Cog, Bell, BellDot } from "lucide-react";
+import UserNav from "./userNav";
+import { useState, useEffect } from 'react';
 import clsx from "clsx";
+
+
 
 const links = [
     {
         name: "dashboard",
-        href: "/dashboard",
+        path: "/dashboard",
+        icon: Home
     },{
         name: "settings",
-        href: "/settings",
-    }
+        path: "/dashboard/settings",
+        icon: Cog
+    },
+    {
+        name: "notifications",
+        path: "/dashboard/notifications",
+        icon: Bell
+    },
 ]
+
 export default function Nav() {
-    const pathname = usePathname();
     return (
-        <>
-            {links.map((link) => (
-                <Link href={link.href} key={link.name}
-                className={clsx('px-4 py-2',
-                    { 'bg-sky-100 text-blue-600' : pathname === link.href })}
-                >
-                    <p>{link.name}</p>
-                </Link>
-            ))}
-        </>
+        <div className="flex flex-row justify-between items-center px-4 py-2">
+            <div className="flex flex-row items-center">
+                <div className="flex flex-row items-center ml-4">
+                    {links.map((link) => (
+                        <Link href={link.path} key={link.name} className={clsx("flex flex-row items-center px-2 py-1 rounded-lg transition duration-200 ease-in-out hover:bg-gray-200", usePathname() === link.path ? "bg-gray-200" : "")}>
+                                <link.icon className="w-5 h-5" />
+                        </Link>
+                    ))}
+                </div>
+            </div>
+            <UserNav />
+        </div>
     );
 }
